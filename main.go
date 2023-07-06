886c756ca5d4042b54cb784bf61be94dea76ec47
OctopusLian <zoctopus@qq.com>
2023-07-06 22:27:19 +0800

    feat(collect): 接口抽象，模拟浏览器访问

--- main.go
+++ main.go
@@ -1,61 +1,31 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
-	"io"
-	"net/http"
+	"gocrawler/collect"
 )
 
 func main() {
-	url := "https://www.thepaper.cn/"
-	body, err := Fetch(url)
-
+	url := "https://book.douban.com/subject/1007305/"
+	var f collect.Fetcher = collect.BrowserFetch{}
+	body, err := f.Get(url)
 	if err != nil {
 		fmt.Println("read content failed:%v", err)
 		return
 	}
+	fmt.Println(string(body))
 
-	// 解析 HTML 文本
+	// 加载HTML文档
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
 	if err != nil {
 		fmt.Println("read content failed:%v", err)
 	}
 
 	doc.Find("div.news_li h2 a[target=_blank]").Each(func(i int, s *goquery.Selection) {
+		// 获取匹配元素的文本
 		title := s.Text()
 		fmt.Printf("Review %d: %s\n", i, title)
 	})
-}
-
-func Fetch(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%d", resp.StatusCode)
-	}
-	bodyReader := bufio.NewReader(resp.Body)
-	e := DeterminEncoding(bodyReader)
-	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
-	return io.ReadAll(utf8Reader)
-}
-
-// 检测并返回当前 HTML 文本的编码格式
-func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
-	bytes, err := r.Peek(1024)
-	if err != nil {
-		fmt.Println("fetch error:%v", err)
-		return unicode.UTF8
-	}
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
 }
