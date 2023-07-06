a5edf86882d74bfc085d046cd0c79ec3125b91f0
OctopusLian <zoctopus@qq.com>
2023-07-06 19:08:52 +0800

    docs: 需求分析和架构图

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module gocrawler
 
 go 1.20
+
+require (
+	golang.org/x/net v0.12.0
+	golang.org/x/text v0.11.0
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := "https://www.chinanews.com.cn/"
+	body, err := Fetch(url)
+
+	if err != nil {
+		fmt.Println("read content failed:%v", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+// 检测并返回当前 HTML 文本的编码格式
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		fmt.Println("fetch error:%v", err)
+		return unicode.UTF8
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
